ea9b8fbc7f6fe4dca5e9bd6128e35eb070e9231c
Yiğit İğci <imyiit@outlook.com>
2023-06-12 13:47:06 +0300

    Update

--- test/index.ts
+++ test/index.ts
@@ -1,12 +1,22 @@
 import Sagdb from "../src";
 
 const db = new Sagdb<string, false>();
-db.on("set", (key, data) => {
-  console.log(key, data);
+db.on("set", (key, new_data) => {
+  console.log({ key, new_data });
+});
+
+db.on("update", (key, old_data, new_data) => {
+  console.log({ key, old_data, new_data });
 });
 
 db.on("delete", (key) => {
   console.log(key);
 });
 
-db.set("a", "b");
+db.set("fruit", "apple");
+db.update("fruit", (old_data) => {
+  if (old_data === "apple") {
+    return "banana";
+  }
+  return "orange";
+});
